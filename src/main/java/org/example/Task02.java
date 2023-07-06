5e52c12b9c9c6f53fcb9583fc22cdd70ab6cf604
NemiaQ <nemia.me@gmail.com>
2023-07-06 23:16:02 +0300

    HW01 added

---
+++ src/main/java/org/example/Task02.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Task02 {
+    //    Простая задача 2: Обработка некорректного ввода
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+
+        System.out.print("Введите ваш возраст: ");
+
+        try {
+            int age = iScanner.nextInt();
+            System.out.println(age);
+        } catch (NumberFormatException | InputMismatchException ex){
+            System.out.println("Некорректный ввод");
+        }
+    }
+}
